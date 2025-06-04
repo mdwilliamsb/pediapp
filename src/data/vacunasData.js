164ff6de82d1df69ee59455a666f097075a4c2b3
Williams <williams@192.168.1.17>
2025-06-04 03:15:49 -0600

    Actualización de PediApp

---
+++ src/data/vacunasData.js
@@ -0,0 +1,26 @@
+// src/data/vacunasData.js
+
+export const VACUNAS_MEXICO = [
+    { id: "bcg", nombre: "BCG", edad: "Recién nacido" },
+    { id: "hepb", nombre: "Hepatitis B", edad: "Recién nacido, 2 y 6 meses" },
+    { id: "pentavalente", nombre: "Pentavalente Acelular", edad: "2, 4, 6 y 18 meses" },
+    { id: "rotavirus", nombre: "Rotavirus", edad: "2 y 4 meses" },
+    { id: "neumococo", nombre: "Neumococo conjugada", edad: "2, 4 y 12 meses" },
+    { id: "influenza", nombre: "Influenza", edad: "6 y 7 meses, anual hasta 59 meses" },
+    { id: "srp", nombre: "SRP (Triple viral)", edad: "1 y 6 años" },
+    { id: "dpt", nombre: "DPT refuerzo", edad: "4 años" }
+  ]
+  
+  export const VACUNAS_USA = [
+    { id: "hepb-usa", nombre: "Hepatitis B", edad: "Nac., 1–2 y 6–18 meses" },
+    { id: "rv", nombre: "Rotavirus (RV)", edad: "2 y 4 meses (y 6 si RotaTeq)" },
+    { id: "dtap", nombre: "DTaP", edad: "2, 4, 6, 15–18 meses y 4–6 años" },
+    { id: "hib", nombre: "Hib", edad: "2, 4, (6), 12–15 meses" },
+    { id: "pcv13", nombre: "Neumococo (PCV13)", edad: "2, 4, 6, 12–15 meses" },
+    { id: "ipv", nombre: "Polio (IPV)", edad: "2, 4, 6–18 meses y 4–6 años" },
+    { id: "mmr", nombre: "MMR", edad: "12–15 meses, 4–6 años" },
+    { id: "var", nombre: "Varicela", edad: "12–15 meses, 4–6 años" },
+    { id: "meningococo", nombre: "Meningococo ACWY", edad: "11–12 años, 16 años" },
+    { id: "vph", nombre: "VPH (HPV)", edad: "11–12 años" },
+  ]
+  
